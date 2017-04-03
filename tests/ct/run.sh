6c0c000525ed6b8bc22bb408dce7be28e5da9dc6
Ant Weiss <anton@otomato.link>
2017-04-03 15:55:13 +0000

    add component test

---
+++ tests/ct/run.sh
@@ -0,0 +1,7 @@
+#! /bin/sh -x
+# Component test for the products service. 
+# Very dumb - create an product and verify there's an product for this product id.
+# Assumes a fresh environment
+
+curl -s -X POST -H "Content-Type:application/json" -d '{"name": "raspberryPi", "price": 35, "stock": 3 }' http://${APP_URL}/product
+curl -s http://${APP_URL}/product | grep raspberryPi
